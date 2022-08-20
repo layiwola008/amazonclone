66f9bd02943d1f7850f3a1996af40bce2d066b06
Olayiwola Adepoju <78272392+layiwola008@users.noreply.github.com>
2022-08-21 00:55:01 +0100

    Update axios.js
    change to your firebase cloud functions URL

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
     baseURL: 'http://localhost:5001/challenge-87efa/us-central1/api'
-    // baseURL: 'https://us-central1-challenge-87efa.cloudfunctions.net/api'
+    // set the link to your firebase function url 
 });
 
 export default instance;
