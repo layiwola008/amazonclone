f2bb2ecd71de66c4ac73d5b9604302e36f48f2e3
Olayiwola Adepoju <78272392+layiwola008@users.noreply.github.com>
2022-08-21 01:29:38 +0100

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
-const stripe = require("stripe")("sk_test_51LQjKEE9YQAk8HvgL1vCL2DVvioYVwVIHx2j44aHOeV0YU5IUCnrKeAv79fbxz8IGy7ldaadCXCJo2IH0mTLZlkN00C3lApOsv");
+const stripe = require("stripe")("add your stripe secret key");
 
 // API
 
